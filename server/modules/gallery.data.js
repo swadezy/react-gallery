ebf50c4f8e957481d0564f83e30366265889686b
Sean Wade <smwade1115@gmail.com>
2021-01-17 13:08:58 -0600

    added a few more photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/Prof_Pic.jpg', description: 'Photo of me at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/With_Friends.jpg', description: 'Photo of my best friends and I.', likes: 0 },
-    { id: 3, path: 'images/Back_in_the_90s.jpg', description: 'Photo of my siblings and I as kids.', likes: 0 },
-    { id: 4, path: 'images/Sis_Got_Married.jpg', description: `Photo of my siblings and I at my sister's wedding.`, likes: 0 },
-    { id: 5, path: 'images/With_Dad_and_Baby.jpg', description: 'Photo of my dad and I with my nephew.', likes: 0 },
+    { id: 1, path: 'images/Prof_Pic.jpg', description: 'At a cabin on a lake at Glacier National Park', likes: 0 },
+    { id: 2, path: 'images/With_Friends.jpg', description: 'My best friends and I in Los Angeles', likes: 0 },
+    { id: 3, path: 'images/Back_in_the_90s.jpg', description: 'Essence of the 90s - my siblings and I as kids', likes: 0 },
+    { id: 4, path: 'images/Sis_Got_Married.jpg', description: `My siblings and I at my sister's wedding`, likes: 0 },
+    { id: 5, path: 'images/With_Dad_and_Baby.jpg', description: 'My dad and I with my nephew', likes: 0 },
     { id: 6, path: 'images/The_Ocean.png', description: 'Photo of me thinking about the ocean.', likes: 0 },
+    { id: 7, path: 'images/Skydiving.png', description: 'My first time skydiving, highly recommend', likes: 0 },
+    { id: 8, path: 'images/True_Self.png', description: 'What I would look like if x-rayed', likes: 0 },
+    { id: 9, path: 'images/With_Gparents.jpg', description: 'What a goof! With my grandparents', likes: 0 },
+    { id: 10, path: 'images/With_the_Fam.jpg', description: 'Up north near Hackensack with my family', likes: 0 }
 ];
 
 module.exports = galleryItems;
